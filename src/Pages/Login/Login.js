f8565dffc8a1b937558c8972849d241096808edd
Faysal Akbar <faysalrj361@gmail.com>
2023-01-09 01:03:28 +0600

    firebase setup

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import auth from '../../firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
-const login = () => {
+const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if(user){
+        console.log(user);
+    }
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className='flex justify-center items-center h-screen'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+                <h2 className="text-2xl font-bold text-center">Please Login</h2>
+                <label className="label">
+                    <span className="label-text">Email</span>
+                </label>
+                <input type="text" className="input input-bordered w-full max-w-xs" />
+                <label className="label">
+                    <span className="label-text">Email</span>
+                </label>
+                <input type="text" className="input input-bordered w-full max-w-xs" />
+                <button className="btn btn-primary uppercase">login...</button>
+                <div className="divider">OR</div>
+                <button 
+                className="btn btn-outline uppercase"
+                onClick={() => signInWithGoogle()}
+                >Continue with google</button>
+            </div>
+            </div>
         </div>
     );
 };
 
-export default login;
+export default Login;
